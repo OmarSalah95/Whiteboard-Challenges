f383c83f97e1b6f3350dbe35e410b6aa26558467
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-05-16 11:28:39 -0700

    Fix to head

--- whiteboardChellenges.js
+++ whiteboardChellenges.js
@@ -9,7 +9,6 @@ console.log(reverse([ 9, 9, 2, 3, 4 ])); // [4, 3, 2, 9, 9]
 
 console.log(reverse([])); // []
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 // Word Endings
 
 // Create a function that adds a string ending to each member in an array.
@@ -18,9 +17,7 @@ const addEnding = (wordsArr, suffix) => wordsArr.map((word) => word + suffix);
 
 //   Examples
 console.log(addEnding([ 'clever', 'meek', 'hurried', 'nice' ], 'ly')); //["cleverly", "meekly", "hurriedly", "nicely"]
-
 console.log(addEnding([ 'new', 'pander', 'scoop' ], 'er')); //["newer", "panderer", "scooper"]
-
 console.log(addEnding([ 'bend', 'sharpen', 'mean' ], 'ing')); //["bending", "sharpening", "meaning"]
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Add up the Numbers from a Single Number
